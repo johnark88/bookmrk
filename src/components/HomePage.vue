5c166860e79f9701e48c39fb838e230bd7bfc663
John Arkema <john.arkema@gmail.com>
2018-10-23 22:38:26 -0500

    Figuring out draggable

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,69 +1,165 @@
 <template>
   <div class="HomePage">
-    <b-container fluid>
+    <b-container>
+      <b-row>
+        <div>
+          <div>
+            <h3>Sortable control</h3>
+          </div>
+          <div>
+            <div>
+              <label><input type="checkbox" v-model="editable">Enable drag and drop</label>
+            </div>
+            <button type="button" class="btn btn-default" @click="orderList">Sort by original order</button>
+          </div>
+        </div>
+      </b-row>
+      <b-row>
+        <b-col>
+          <draggable class="list-group" element="ul" v-model="list" :options="dragOptions" :move="onMove" @start="isDragging=true"
+            @end="isDragging=false">
+            <transition-group type="transition" :name="'flip-list'">
+              <li class="list-group-item" v-for="element in list" :key="element.order">
+                <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed"
+                  aria-hidden="true"></i>
+                {{element.name}}
+                <span class="badge">{{element.order}}</span>
+              </li>
+            </transition-group>
+          </draggable>
+        </b-col>
+
+        <b-col>
+          <draggable element="span" v-model="list2" :options="dragOptions" :move="onMove">
+            <transition-group name="no" class="list-group" tag="ul">
+              <li class="list-group-item" v-for="element in list2" :key="element.order">
+                <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed"
+                  aria-hidden="true"></i>
+                {{element.name}}
+                <span class="badge">{{element.order}}</span>
+              </li>
+            </transition-group>
+          </draggable>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <div class="list-group col-md-3">
+            <pre>{{listString}}</pre>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="list-group col-md-3">
+            <pre>{{list2String}}</pre>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-import draggable from 'vuedraggable';
-
-export default {
-  name: 'HomePage',
-  components: {
-    draggable,
-  },
-  data() {
-    return {
-      editable: true,
-      isDragging: false,
-      delayedDragging: false,
-    };
-  },
-  methods: {
-    onMove({
-      relatedContext,
-      draggedContext,
-    }) {
-      const relatedElement = relatedContext.element;
-      const draggedElement = draggedContext.element;
-      return (
-        (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
-      );
+  import draggable from "vuedraggable";
+  const message = [
+    "vue.draggable",
+    "draggable",
+    "component",
+    "for",
+    "vue.js 2.0",
+    "based",
+    "on",
+    "Sortablejs"
+  ];
+  export default {
+    name: "hello",
+    components: {
+      draggable
     },
-  },
-  computed: {
-    dragOptions() {
-      return {};
+    data() {
+      return {
+        list: message.map((name, index) => {
+          return {
+            name,
+            order: index + 1,
+            fixed: false
+          };
+        }),
+        list2: [],
+        editable: true,
+        isDragging: false,
+        delayedDragging: false
+      };
     },
-  },
-};
+    methods: {
+      orderList() {
+        this.list = this.list.sort((one, two) => {
+          return one.order - two.order;
+        });
+      },
+      onMove({
+        relatedContext,
+        draggedContext
+      }) {
+        const relatedElement = relatedContext.element;
+        const draggedElement = draggedContext.element;
+        return (
+          (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+        );
+      }
+    },
+    computed: {
+      dragOptions() {
+        return {
+          animation: 0,
+          group: "description",
+          disabled: !this.editable,
+          ghostClass: "ghost"
+        };
+      },
+      listString() {
+        return JSON.stringify(this.list, null, 2);
+      },
+      list2String() {
+        return JSON.stringify(this.list2, null, 2);
+      }
+    },
+    watch: {
+      isDragging(newValue) {
+        if (newValue) {
+          this.delayedDragging = true;
+          return;
+        }
+        this.$nextTick(() => {
+          this.delayedDragging = false;
+        });
+      }
+    }
+  };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.column {
-  padding: 1rem;
-  background-color: #33b5e5;
-  border: 2px solid #fff;
-  color: #fff;
-  text-align: center;
-}
-  h3 {
-    margin: 40px 0 0;
+<style>
+  .flip-list-move {
+    transition: transform 0.5s;
   }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
+  .no-move {
+    transition: transform 0s;
   }
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
+  .ghost {
+    opacity: 0.5;
+    background: #c8ebfb;
   }
 
-  a {
-    color: #42b983;
+  .list-group {
+    min-height: 20px;
+  }
+
+  .list-group-item {
+    cursor: move;
+  }
+
+  .list-group-item i {
+    cursor: pointer;
   }
 </style>
